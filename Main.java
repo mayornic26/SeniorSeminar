7dfbc61acb19b98d672ee1c2fd79fe5d42b8c621
mayornic26 <mayornic26@7hills.org>
2025-04-11 09:11:33 -0400

    Set up VScode and login

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class Main {
+  public static void main(String[] args) {
+    
+    
+    try {
+      File myObj = new File("SrSeminar_RawData.csv");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+      
+    }
+  }
+}
