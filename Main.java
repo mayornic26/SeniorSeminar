1d175594515335b2ce48e528caeff566a17d7abf
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2025-04-15 09:35:00 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
         SeniorSeminar p1 = new SeniorSeminar();
         
-        p1.PrintStudentList();
+        //p1.PrintStudentList();
         p1.tallySessions();
         p1.PrintPresenterID();
     }
