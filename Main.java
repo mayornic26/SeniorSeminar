13d5a3dcb1eac59db094bd05c0ad2f00504500ce
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2025-04-21 19:39:56 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,15 @@
+/**
+ * Main.java
+ * 
+ * Author: Nico Mayoral
+ * Date: 4/21/25
+ * Preconditons: Main  Class and Object
+ * Postconditions: uses SessionSeminar.java, Sessions.java and Student.java  and calls the methods available
+ * 
+ * Purpose: This object is used as a tester for the SessionSeminar class able to execute all needed methods
+ * 
+ */
+
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,9 +19,9 @@ import java.util.ArrayList;
 public class Main {
 	public static void main(String[] args) {
         SeniorSeminar p1 = new SeniorSeminar();
-        
-        //p1.PrintStudentList();
-        p1.tallySessions();
-        p1.PrintPresenterID();
+        p1.tallySessions();        
+        p1.ScheduleSessions();
+        p1.PrintRosterPerSessions();
+        p1.PrintRosterforStudent();
     }
 }
