ba923c34ccb9183310624c43b7c2a77567f25d73
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2025-04-25 08:16:22 -0400

    Update Student.java
    comments

--- Student.java
+++ Student.java
@@ -22,10 +22,10 @@ public class Student {
         private int c2;
         private int c3;
         private int c4;
-        private int c5;
+        private int c5; 
         
-      // Variables to tell if the student has been scheduled for each of their chocies
-        
+          // Variables to tell if the student has been scheduled for each of their chocies
+         //no sessions are scheduled at start
         private boolean S1;
         private boolean S2;
         private boolean S3;
@@ -33,15 +33,14 @@ public class Student {
         private boolean S5;
        
        // varibles to tell per class which timeslot is scheduled
-       
+       // No timeslots are booked to start
         private boolean timeslot1;
         private boolean timeslot2;
         private boolean timeslot3;
         private boolean timeslot4;
         private boolean timeslot5;
         
-        // varibles to tell if the student will attend the class on the 2nd run 
-        
+        // varibles to tell if the student will attend the class on the 2nd run     
         private boolean secondary1;
         private boolean secondary2;
         private boolean secondary3;
@@ -53,24 +52,27 @@ public class Student {
         public Student (String studentName, int choiceOne, int choiceTwo, int choiceThree, int choiceFour, int choiceFive)
         {
             name = studentName;
-            c1 = choiceOne;
+            c1 = choiceOne; 
             c2 = choiceTwo;
             c3 = choiceThree;
             c4 = choiceFour;
             c5 = choiceFive;
             
-            //set them all to false to turn true when they are placed
+            // To start no sessions are scheduled
             S1 = false;
             S2 = false;
             S3 = false;
             S4 = false;
             S5 = false;
             
+             // No timeslots are booked to start
             timeslot1 = false;
             timeslot2 = false;
             timeslot3 = false;
             timeslot4 = false;
-            timeslot5 = false;       
+            timeslot5 = false;  
+             
+             // No secondary placements to start     
             secondary1 = false;
             secondary2 = false;
             secondary3 = false;
@@ -84,19 +86,19 @@ public class Student {
             return name;
         }
              
-        //getchoice checks the numbers to the choices of the classes
+        //getchoice sets the numbers to the choices of the classes
         public int getchoice(int number) {
-		   if(number ==1  ) {
-			   return c1;
-		   } else if (number == 2) {
-			   return c2;   
+		   if(number == 1) {
+			   return c1; //sets the number 1 to c1 which is B in exel
+		   } else if (number == 2) {  //second choice
+			   return c2;    
 	       } else if (number == 3) {
 			   return c3;   
 		   } else if (number == 4) {
 	           return c4;
 	       } else if (number == 5) {
 			  return c5; 
-		   }  else return 0;
+		   }  else return 0; //dosnt work
 			   	
 		} 
 		
@@ -116,7 +118,8 @@ public class Student {
 			   	
 		} 
 
-		//turns the falses into true if sessionNumber = choice one and if they are scheduled
+		//turns the falses into true if the sessionID matches one of the students choices
+		//and is primary or secondary
 		public boolean is_scheduled(int sessionNumber){
 			return ((sessionNumber == c1) && S1) 
 			|| ((sessionNumber == c2) && S2)
@@ -126,6 +129,7 @@ public class Student {
 			
 		}
 		
+		// returns true if the session is scheduled as the students primary (not secondary).
 		public boolean is_scheduled_primary(int sessionNumber){
 			return ((sessionNumber == c1) && S1 && !secondary1) 
 			|| ((sessionNumber == c2) && S2 && !secondary2)
@@ -135,6 +139,7 @@ public class Student {
 			
 		}
 		
+		//returns true if the session is scheduled as the students secondary offering.
 		public boolean is_scheduled_secondary(int sessionNumber){
 			return ((sessionNumber == c1) && S1 && secondary1) 
 			|| ((sessionNumber == c2) && S2 && secondary2)
@@ -148,6 +153,7 @@ public class Student {
 		
 		//setters
 		//checks to see if the choice has been selected making it true
+		//makes one of the students 5 choices as scheudled in the primary offering
 		public void mark_as_scheduled_primary(boolean value,int slot){
 			if (slot == 1) {
 			   S1 = value;
@@ -163,6 +169,7 @@ public class Student {
 		}
 		
 	   //the class has been scheduled on the alternate scheudle 
+	   ////makes one of the students 5 choices as scheudled in the secondary offering
 		public void mark_as_scheduled_secondary(boolean value,int slot){
 			if (slot == 1) {
 			   S1 = value;
@@ -183,6 +190,7 @@ public class Student {
 		}
 		
 		//set at what time the class is going to be taught
+		//blocks out one of the student’s five timeslots in their personal schedule.
 		public void settimeslot(boolean value,int slot){
 			if (slot == 1) {
 			   timeslot1 = value;
@@ -198,6 +206,7 @@ public class Student {
 		}
 			
 		// Checks to see if the student is booked for that time in class
+		//checks whether the students scheudle already has that timeslot filled.
 		public boolean istimeslotbooked(int slot){
 			if (slot == 1) {
 			   return timeslot1;
